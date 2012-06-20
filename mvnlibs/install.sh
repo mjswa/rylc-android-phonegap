5af1701a321c2616c51eef2fe93687f1f6b7c691
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2012-06-20 16:47:52 +0200

    initial commit

---
+++ mvnlibs/install.sh
@@ -0,0 +1,6 @@
+mvn install:install-file  -Dfile=cordova-1.8.1.jar \
+                          -DgroupId=org.apache.cordova \
+                          -DartifactId=cordova \
+                          -Dversion=1.8.1 \
+                          -Dsources=cordova-1.8.1-sources.jar \
+                          -Dpackaging=jar
---
+++ src/main/java/com/opitzconsulting/rylc/BarcodePlugin.java
@@ -0,0 +1,44 @@
+package com.opitzconsulting.rylc;
+
+import android.app.Activity;
+import android.content.Intent;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import org.apache.cordova.api.Plugin;
+import org.apache.cordova.api.PluginResult;
+import org.json.JSONArray;
+
+/**
+ * @author Tobias Bosch (OPITZ CONSULTING GmbH)
+ */
+public class BarcodePlugin extends Plugin {
+  private String callbackId;
+
+  @Override
+  public PluginResult execute(String jsAction, JSONArray jsParams, String callbackId) {
+    this.callbackId = callbackId;
+    IntentIntegrator integrator = new IntentIntegrator((Activity) this.ctx) {
+      @Override
+      protected void startActivityForResult(Intent intent, int code) {
+        ctx.startActivityForResult(BarcodePlugin.this, intent, code);
+      }
+    };
+    integrator.initiateScan();
+
+    return new PluginResult(PluginResult.Status.NO_RESULT);
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    super.onActivityResult(requestCode, resultCode, intent);
+    IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+    if (scanResult == null) {
+      return;
+    }
+    if (resultCode == Activity.RESULT_OK) {
+      this.success(scanResult.getContents(), this.callbackId);
+    } else {
+      this.error("Error", this.callbackId);
+    }
+  }
+}
